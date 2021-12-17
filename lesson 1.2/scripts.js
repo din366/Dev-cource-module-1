ddd2205dcc0f802b888ac17742c68f602289aef9
Alex Din <din366@gmail.com>
2021-12-17 12:42:37 +0300

    commit for 2 lesson, module 1

---
+++ lesson 1.2/scripts.js
@@ -0,0 +1,19 @@
+{
+  const productName = "apples";
+  const productAmount = 35;
+  const productCategory = "fruits";
+  const productPrice = 12;
+
+  console.log(productName);
+  console.log(productAmount * productPrice);
+}
+
+{
+  const productName = "mobile phone";
+  const productAmount = 24;
+  const productCategory = "electronics";
+  const productPrice = 435;
+
+  console.log(`Наименование товара ${productName}`);
+  console.log(`Общая сумма товара: ${productAmount * productPrice}`);
+}
