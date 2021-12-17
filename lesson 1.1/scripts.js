9d7a97f976aaa565afa507e0d97774a579126252
Alex Din <din366@gmail.com>
2021-12-17 12:32:57 +0300

    first commit

---
+++ lesson 1.1/scripts.js
@@ -0,0 +1,7 @@
+const productName = "apples";
+const productAmount = 35;
+const productCategory = "fruits";
+const productPrice = 12;
+
+console.log(productName);
+console.log(productAmount * productPrice);
