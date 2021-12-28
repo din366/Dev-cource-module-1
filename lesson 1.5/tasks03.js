da595ec6b647b8dc32a96264947b8af10cbe3afe
Alex Din <din366@gmail.com>
2021-12-28 11:06:58 +0300

    add lesson 1.5.3 method without reverse

--- lesson 1.5/tasks03.js
+++ lesson 1.5/tasks03.js
@@ -1,3 +1,13 @@
 const getReverseString = (str) => {
   return str.split("").reverse().join("");
 };
+
+const getReverseStringWithoutReverse = (str) => {
+  const returnedArray = [];
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    returnedArray.push(str[i]);
+  }
+
+  return returnedArray.join("");
+};
